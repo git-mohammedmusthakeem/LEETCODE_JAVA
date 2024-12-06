5c263010461f5cc7163d5e1c3315c04ee64c05ee
MOHAMMED MUSTHAKEEM <98821339+git-mohammedmusthakeem@users.noreply.github.com>
2024-12-06 23:32:44 +0530

    Add files via upload

---
+++ 162. Find Peak Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int len = nums.length;
+        if(len == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        for(int i = 1; i < len - 1; i++){
+            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+                return i;
+        }
+        if(nums[len - 1] > nums[len - 2])
+            return len - 1;
+        return -1;
+    }
+}
