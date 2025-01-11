e4fc9cd78e5ae2a983bc6fe6b87a4cb3d04e53f6
MOHAMMED MUSTHAKEEM <98821339+git-mohammedmusthakeem@users.noreply.github.com>
2025-01-11 22:20:40 +0530

    Add files via upload

---
+++ 362. Design Hit Counter.java
@@ -0,0 +1,22 @@
+class HitCounter {
+      private Queue<Integer> q;
+      /** Initialize your data structure here. */
+      public HitCounter() {
+          q = new LinkedList<>();
+      }
+
+      /** Record a hit.
+          @param timestamp - The current timestamp (in seconds granularity). */
+      public void hit(int timestamp) {
+          q.offer(timestamp);
+      }
+
+      /** Return the number of hits in the past 5 minutes.
+          @param timestamp - The current timestamp (in seconds granularity). */
+      public int getHits(int timestamp) {
+          while(!q.isEmpty() && timestamp - q.peek() >= 300){
+              q.poll();
+          }
+          return q.size();
+      }
+  }
